092a910cbd4c412d19db1c20b127e7879fe82224
noobpk <ltp.noobpk@gmail.com>
2024-06-24 22:08:43 +0700

    update config & pipeline

--- README.md
+++ README.md
@@ -26,9 +26,13 @@ Ref: [install-sonatype-nexus-using-docker](https://ahgh.medium.com/how-to-setup-
 
 - `docker pull takeyamajp/ubuntu-sshd`
 
-- `docker run --name ubuntu-train-srv -v /your/home/ubutu-train:/mnt/train-data -e TZ=Asia/Tokyo -e ROOT_PASSWORD=root -p 2222:22 takeyamajp/ubuntu-sshd`
+- `docker run --name ubuntu-sshd  -v /Users/lethanhphuc/UIT/Train:/mnt/train-data -v /var/run/docker.sock:/var/run/docker.sock -e TZ=Asia/Tokyo -e ROOT_PASSWORD=root -p 2222:22 takeyamajp/ubuntu-sshd`
 
 Ref: [ubuntu-sshd](https://hub.docker.com/r/takeyamajp/ubuntu-sshd)
+
+## Setup Ubuntu-Deploy.srv
+
+- `docker run --name ubuntu-deploy  -v /Users/lethanhphuc/UIT/Deploy:/mnt/deploy-data -v /var/run/docker.sock:/var/run/docker.sock -e TZ=Asia/Tokyo -e ROOT_PASSWORD=root -p 2323:22 -p 5555:5000 takeyamajp/ubuntu-sshd`
 
 ### Docker Network Check
 
@@ -48,17 +52,52 @@ Install ssh-steps plugins
 
 #### On Ubuntu-Train.srv
 
-Install Curl & PyPip & Git & JQ
+Install Curl & PyPip & Git & JQ & Trivy
 
 - `ssh root@127.0.0.1 -p 2222`
 
 - `apt update && apt install python3-pip git curl jq -y`
 
-- `/usr/bin/python3 --version`
+- `apt-get install wget apt-transport-https gnupg lsb-release -y`
+
+- `wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | apt-key add -`
+
+- `echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | tee -a /etc/apt/sources.list.d/trivy.list`
+
+- `apt-get update && apt-get install trivy -y`
+
+Verify After Install
+
+- `python3 --version`
 
 - `pip3 --version`
   
 - `git --version`
+
+- `trivy --version`
+
+Install Docker Engine
+
+- `for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg; done`
+
+- `apt-get update && apt-get install ca-certificates -y && install -m 0755 -d /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && chmod a+r /etc/apt/keyrings/docker.asc`
+
+- `
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        tee /etc/apt/sources.list.d/docker.list > /dev/null
+`
+
+- `apt-get update && apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y`
+
+#### On Ubuntu-Deploy.srv
+
+Install Curl & PyPip & Git & JQ
+
+- `ssh root@127.0.0.1 -p 2323`
+
+- `apt update && apt install curl -y`
 
 Install Docker Engine
 
--- code-example/devsecops.groovy
+++ code-example/devsecops.groovy
@@ -2,10 +2,11 @@ pipeline {
     agent any
 
     environment {
-        //Ubuntu
+        //Ubuntu-SSHD
         SSH_SERVER = '172.17.0.4'
         SSH_USER = 'root'
         CREDENTIALS_ID = '7458c9b4-2e01-4ad5-919f-e5e518f8f3ae'
+        IMAGE_DIR = '/tmp/image/'
         GIT_REPO_URL = 'https://github.com/noobpk/MLOps-DevSecOps-Awesome.git'
         GIT_CLONE_DIR = '/tmp/MLOps-DevSecOps-Awesome'
         //Nexus
@@ -13,9 +14,15 @@ pipeline {
         ENCODE_FILE = 'label_encoder.pickle'
         TOKENIZE_FILE = 'tokenizer.pickle'
         NEXUS_URL = 'http://172.17.0.3:8081/repository/'
-        NEXUS_REPO_PATH = 'mlops/'
+        NEXUS_REPO_PATH_MLOPS = 'mlops/'
+        NEXUS_REPO_PATH_DEVSECOPS = 'devsecops/'
         NEXUS_USERNAME = 'admin'
         NEXUS_PASSWORD = 'admin'
+        IMAGE_EXPORT_FILE = 'text-classification-cnn-model.tar'
+        //Ubuntu-Deploy
+        SSH_SERVER_2 = '172.17.0.4'
+        SSH_USER_2 = 'root'
+        CREDENTIALS_ID_2 = '7458c9b4-2e01-4ad5-919f-e5e518f8f3ae'
     }
 
     parameters {
@@ -58,10 +65,11 @@ pipeline {
                         remote.allowAnyHosts = true
 
                         // Construct the download URL
-                        def downloadUrl_0 = "${NEXUS_URL}${NEXUS_REPO_PATH}${params.JOB_MLOPS_BUILD_NUMBER}/text_classification_cnn_model.h5"
-                        def downloadUrl_1 = "${NEXUS_URL}${NEXUS_REPO_PATH}${params.JOB_MLOPS_BUILD_NUMBER}/label_encoder.pickle"
-                        def downloadUrl_2 = "${NEXUS_URL}${NEXUS_REPO_PATH}${params.JOB_MLOPS_BUILD_NUMBER}/tokenizer.pickle"
+                        def downloadUrl_0 = "${NEXUS_URL}${NEXUS_REPO_PATH_MLOPS}${params.JOB_MLOPS_BUILD_NUMBER}/text_classification_cnn_model.h5"
+                        def downloadUrl_1 = "${NEXUS_URL}${NEXUS_REPO_PATH_MLOPS}${params.JOB_MLOPS_BUILD_NUMBER}/label_encoder.pickle"
+                        def downloadUrl_2 = "${NEXUS_URL}${NEXUS_REPO_PATH_MLOPS}${params.JOB_MLOPS_BUILD_NUMBER}/tokenizer.pickle"
                         def FILE_PATH = "${GIT_CLONE_DIR}/code-example/docker-image/"
+
                         // Download artifact
                         sshCommand remote: remote, command: """
                         wget -O ${FILE_PATH}${MODEL_FILE} ${downloadUrl_0}
@@ -89,8 +97,29 @@ pipeline {
                         sshCommand remote: remote, command: """
                         curl -fsSL https://raw.githubusercontent.com/ZupIT/horusec/main/deployments/scripts/install.sh | bash -s latest
                         cd ${GIT_CLONE_DIR}/code-example/docker-image
-                        /root/horusec start -p "./" -e="true" --disable-docker="true"
+                        /root/horusec start -p "./" --disable-docker="true"
                         """
+                    }
+                }
+            }
+        }
+
+        stage('Quality Gate') {
+            steps {
+                script {
+                    // Manual approval step
+                    def userInput = input(
+                        id: 'Proceed', message: 'Do you want to proceed with the next stage?',
+                        parameters: [
+                            booleanParam(defaultValue: true, description: '', name: 'Proceed')
+                        ]
+                    )
+
+                    // Check user input
+                    if (userInput) {
+                        echo "User chose to proceed."
+                    } else {
+                        error "Pipeline aborted by user."
                     }
                 }
             }
@@ -108,10 +137,83 @@ pipeline {
                         remote.password = SSH_PASS
                         remote.allowAnyHosts = true
 
-                        // Install requirements.txt
+                        // Build docker image
                         sshCommand remote: remote, command: """
-                        cd ${GIT_CLONE_DIR}/code-example/image
+                        cd ${GIT_CLONE_DIR}/code-example/docker-image
                         docker build -t text-classification-cnn-model .
+                        docker tag text-classification-cnn-model text-classification-cnn-model:${currentBuild.number}
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Scan Docker Image') {
+            steps {
+                script {
+                    // Retrieve the password from Jenkins secret text credentials
+                    withCredentials([string(credentialsId: CREDENTIALS_ID, variable: 'SSH_PASS')]) {
+                        def remote = [:]
+                        remote.name = 'test'
+                        remote.host = SSH_SERVER
+                        remote.user = SSH_USER
+                        remote.password = SSH_PASS
+                        remote.allowAnyHosts = true
+
+                        // Scan docker image
+                        sshCommand remote: remote, command: """
+                        trivy image text-classification-cnn-model:${currentBuild.number} --scanners vuln
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Upload Image to Nexus') {
+            steps {
+                script {
+                    // Retrieve the password from Jenkins secret text credentials
+                    withCredentials([string(credentialsId: CREDENTIALS_ID, variable: 'SSH_PASS')]) {
+                        def remote = [:]
+                        remote.name = 'test'
+                        remote.host = SSH_SERVER
+                        remote.user = SSH_USER
+                        remote.password = SSH_PASS
+                        remote.allowAnyHosts = true
+
+                        // Construct the upload URL
+                        def uploadUrl_0 = "${NEXUS_URL}${NEXUS_REPO_PATH_DEVSECOPS}${env.BUILD_NUMBER}/${IMAGE_EXPORT_FILE}"
+
+                        // Export image and Upload image to nexus
+                        sshCommand remote: remote, command: """
+                        docker save -o ${IMAGE_DIR}${IMAGE_EXPORT_FILE} text-classification-cnn-model:${currentBuild.number}
+                        curl -v -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} --upload-file ${IMAGE_DIR}${IMAGE_EXPORT_FILE} ${uploadUrl_0}
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Deploy Image to Ubuntu-Deploy.srv') {
+            steps {
+                script {
+                    // Retrieve the password from Jenkins secret text credentials
+                    withCredentials([string(credentialsId: CREDENTIALS_ID, variable: 'SSH_PASS')]) {
+                        def remote = [:]
+                        remote.name = 'test'
+                        remote.host = SSH_SERVER
+                        remote.user = SSH_USER
+                        remote.password = SSH_PASS
+                        remote.allowAnyHosts = true
+
+                        // Construct the download URL
+                        def downloadUrl_0 = "${NEXUS_URL}${NEXUS_REPO_PATH_DEVSECOPS}${env.BUILD_NUMBER}/${IMAGE_EXPORT_FILE}"
+
+                        // Load image and run image
+                        sshCommand remote: remote, command: """
+                        wget -O ${IMAGE_DIR}${IMAGE_EXPORT_FILE} ${downloadUrl_0}
+                        docker load -i  ${IMAGE_DIR}${IMAGE_EXPORT_FILE}
+                        docker run --name text-classification-cnn-model -p 5000:443 --rm
                         """
                     }
                 }
